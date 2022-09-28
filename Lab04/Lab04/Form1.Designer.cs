84278f996f7046e6137a49ca559c62eebad3e03d
Ton <supermouse0985@gmail.com>
2022-09-28 23:53:11 +0700

    Thêm các vị trí bị sai

--- Lab04/Lab04/Form1.Designer.cs
+++ Lab04/Lab04/Form1.Designer.cs
@@ -68,9 +68,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(306, 43);
+            this.label1.Location = new System.Drawing.Point(324, 43);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(427, 36);
+            this.label1.Size = new System.Drawing.Size(427, 45);
             this.label1.TabIndex = 0;
             this.label1.Text = "Quản Lý Thông Tin Sinh Viên";
             // 
